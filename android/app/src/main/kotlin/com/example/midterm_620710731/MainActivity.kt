b485650a6b58cd645a4a621c436e4fc6193d2841
Rujirada Klawikkit <klawikkit_r@silpakorn.edu>
2022-01-16 10:51:02 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/midterm_620710731/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.midterm_620710731
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:midterm_620710731/pig_cal.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.pink,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var weghtCal = WeightCalcualte();
+
+  final _controller = TextEditingController();
+  final _controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/bg.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("PIG WEIGHT",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.pink.shade400),
+                      ),
+                      Text("CALCULATOR",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.pink.shade400
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image(image: AssetImage('assets/images/pig.png'), height: 160.0,),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Container(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text("LENGTH"),
+                                  Text("(cm)"),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: TextField(
+                                      controller: _controller,
+                                      textAlign: TextAlign.center,
+                                      decoration: InputDecoration(
+                                        hintText: 'Enter length',
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text("GIRTH"),
+                                  Text("(cm)"),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: TextField(
+                                      controller: _controller2,
+                                      textAlign: TextAlign.center,
+                                      decoration: InputDecoration(
+                                        hintText: 'Enter length',
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('CALCULATE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ),
+                        onPressed: (){
+                          var length = double.tryParse(_controller.text);
+                          var girth = double.tryParse(_controller2.text);
+                          if(length == null || girth == null){
+                            showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('ERROR'),
+                                  content: Text("Invalid input"),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }else{
+                            var weight = weghtCal.calculateWeight(length/100, girth/100);
+                            var Weight2 = weight-3;
+                            var Weight3 = weight+3;
+                            var Price = (Weight2*112.50).round();
+                            var Price2 = (Weight3*112.50).round();
+
+                            showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('RESULT'),
+                                  content: Text("Weight: $Weight2 - $Weight3 kg\nPrice: $Price - $Price2 Baht"),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+      )
+      ,
+    );
+  }
+}
